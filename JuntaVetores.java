4a13c265a4380322dc497d330d5a63fdd7117435
nayaraporto <naya08.vitoria@gmail.com>
2025-05-21 12:29:34 -0300

    Update JuntaVetores.java

--- JuntaVetores.java
+++ JuntaVetores.java
@@ -3,31 +3,25 @@ import java.util.Scanner;
 public class JuntaVetores {
 
 	public static void main(String[] args) {
-		
-		Scanner ler = new Scanner(System.in);
-		int a[] = new int[2];
-		int b[] = new int[2];
-		int c[] = new int[4];
-		
-		for(int i=0; i<2; i++) {
-			System.out.println("Informe o " + i + " valor ..:");
-			a[i] = ler.nextInt();
-		}
-		
-        for(int i=0; i<2; i++) {
-        System.out.println("Informe o " + i + " valor ..:");
-		b[i] = ler.nextInt();
-		
-	}
-	
-	    for(int i=0; i<2; i++) {
-	    	c[i] = a[i];
-	    	c[i] = b[i];
-	    }
-	    for(int i=0; i<4; i++) {
-	    	System.out.println("Informe o " + i + " valor..:" + c[i]);
-	    }
-        ler.close();
-	}
+	Scanner ler = new Scanner(System.in);
 
+int a[] = new int[2];
+for (int i = 0; i < 2; i++) {
+System.out.println("Informe o " + i + "º valor..:");
+a[i] = ler.nextInt();
 }
+int b[] = new int[2], c[] = new int[4];
+for (int i = 0; i < 2; i++) {
+System.out.println("Informe o " + i + "º valor..:");
+b[i] = ler.nextInt();
+
+c[i] = a[i];
+c[i + 2] = b[i];
+}
+ler.close();
+for (int i = 0; i < 4; i++) {
+System.out.println("O valor" + i + "° é..:" + c[i]);
+}
+}
+}
+
